41c2410956dbaa5100cd7979333d99f4bb1c9648
f-sanges <63789710+f-sanges@users.noreply.github.com>
2020-11-05 21:51:13 +0100

    modified "working with strings"

--- working_with_strings.py
+++ working_with_strings.py
@@ -1,5 +1,10 @@
 s = 'Hello'
 
-print(s.upper())
+print(s.upper())  # uppercase
 
-print(s.lower())
+print(s.lower())  # lowercase
+
+print(s.replace("l", "c"))  # replace
+print(s.replace("l", "c", 1))  # replace only one occurency
+
+print(s.count("l"))  # count
