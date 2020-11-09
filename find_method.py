6fed26a9f044d4c4a11fd8e5727bf1b93a963f69
f-sanges <63789710+f-sanges@users.noreply.github.com>
2020-11-09 23:37:22 +0100

    added new file find_method

---
+++ find_method.py
@@ -0,0 +1,45 @@
+value = "cat picture is cat picture plus picture"
+
+# Find first index of the string
+
+i = value.find("picture")
+print("First occurrence of picture: " + str(i))
+
+c = value.rfind("picture")  # rfind
+print("rfind di picture: " + str(c))
+
+# Find first index of this string after previous index
+b = value.find("picture", i + 1)
+print("Second occurrence of picture: " + str(b))
+
+# loop to find the index of every value in a string
+location = 0
+while True:
+    location = value.find("picture", location + 1)
+
+    if location == -1:
+        break
+    print("location: " + str(location))
+
+
+# Other method to loop to find the index of every value in a string
+value2 = "cat picture is cat picture plus picture"
+locazione = len(value2)
+print("length of value2: " + str(locazione))
+while True:
+
+    locazione = value2.rfind("picture", 0, locazione)
+    if locazione == -1:
+        break
+    print("locazione: " + str(locazione))
+
+
+
+
+
+
+# print(value.find("daog"))
+if value.find("dog") == -1:
+    print("not found")
+else:
+    print("found")
