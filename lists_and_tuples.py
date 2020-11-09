12f4664aa74b510ac2ecddcf3cbd3ed949461823
f-sanges <63789710+f-sanges@users.noreply.github.com>
2020-11-09 21:52:19 +0100

    modified lists_and_tuples file

--- lists_and_tuples.py
+++ lists_and_tuples.py
@@ -6,10 +6,10 @@ length = len(list1)
 
 print(list1[1:4])
 
-print("list1 length: " + str(length))       # length
+print("list1 length: " + str(length))  # length
 
-print("Index of the element 'quattro': " + str(list1.index('quattro')))   # index
-del list1[3]                # delete an element
+print("Index of the element 'quattro': " + str(list1.index('quattro')))  # index
+del list1[3]  # delete an element
 print("list1 after deletion of the third element: " + str(list1))
 
 list1.insert(3, 'quattro')
@@ -18,21 +18,29 @@ print("list1 after insert of the element 'quattro' at the 3th position: " + str(
 list2 = list1 * 2  # repetition
 print(list2)
 
-list3 = [x**2 for x in range(1, 11)]
+list3 = [x ** 2 for x in range(1, 11)]
 print(list3)
 
-# tuple
+# list comprehension
+
+a = [1, 2, 3, 4, 5]
+b = []
+for numbers in a:
+    b.append(numbers)
+print("list b: " +str(b))
+
+# ====================== tuple ======================
 
 tup1 = (1, 2, 'tre')
 print(tup1)
 
-tup2 = (4,)                 # the comma at the end is necessary
+tup2 = (4,)  # the comma at the end is necessary
 tup3 = tup1 + tup2
 print(tup3)
-for element in tup3:        # for loop
+for element in tup3:  # for loop
     print(element)
 
 for elements in [0, 1, 2]:
     print('xxx')
-del tup3                    # delete a tuple
+del tup3  # delete a tuple
 # print(tup3)
