4b67d096a0a0352618b2796d4c4232ac94746920
f-sanges <63789710+f-sanges@users.noreply.github.com>
2020-11-10 16:54:34 +0100

    added fibonacci_module and fibonacci_module_main

---
+++ fibonacci_module_main.py
@@ -0,0 +1,6 @@
+'''A module is a Python object with arbitrarily named attributes that you can bind and reference
+A module can define functions, classes and variables'''
+
+import fibonacci_module
+
+fibonacci_module.fib(1000)
