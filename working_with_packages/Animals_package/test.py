d7787eeddec90d8b28e8792cb6eef1c48719019e
f-sanges <63789710+f-sanges@users.noreply.github.com>
2020-11-10 21:10:20 +0100

    added files for package example

---
+++ working_with_packages/Animals_package/test.py
@@ -0,0 +1,13 @@
+from working_with_packages.Animals_package.Mammals import Mammals   #NB: working_with_packages.Animals_package.Mammals
+from working_with_packages.Animals_package.Birds import Birds
+
+mymammal = Mammals()
+mymammal.printMembers()
+
+mybird = Birds()
+mybird.printMembers()
+
+
+
+
+
---
+++ working_with_packages/packages_note.py
@@ -0,0 +1,3 @@
+'''Packages are namespaces which contain multiple packages and modules themselves
+Each package in Python is a directory which must contain a special file called __init__.py'''
+
